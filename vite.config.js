6df6aefed5bd33ec5f030641f822d12d5cc3804d
gregsadetsky <lepetitg@gmail.com>
2023-09-29 19:23:47 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 export default {
-  // why??
-  // https://github.com/vitejs/vite/discussions/9332#discussioncomment-4387303
-  base: "./",
   assetsInclude: ["**/*.obj", "**/*.mtl"],
   build: {
     outDir: "docs",
